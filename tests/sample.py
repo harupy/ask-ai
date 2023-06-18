50bd0c7303fcb1f3b18584ae328c3db62fd0c665
harupy <hkawamura0130@gmail.com>
2023-06-18 12:37:11 +0900

    update sample.py

--- tests/sample.py
+++ tests/sample.py
@@ -1,18 +1,5 @@
-def f():
-    arr = []
-    for i in range(10):
-        arr.append(2 * i)
-    return arr
-
-
-def read(p: str):
-    f = open(p, "r")
-    return f.read()
-
-
 def main():
-    arr = f()
-    print(arr)
+    print("hello world")
 
 
 if __name__ == "__main__":
